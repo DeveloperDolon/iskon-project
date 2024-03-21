f66a521d8809f8a459cf74c6a499260ce11d7275
Dolon Chandra Roy <dolonr718@gmail.com>
2024-03-21 23:10:35 +0600

    home page design complete commit

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -15,19 +15,19 @@ const NavBar = () => {
     const [isOpen, setIsOpen] = useState<Boolean>(false);
 
     const navItems = <>
-        <li className='transition-all duration-100 hover:border-b-2 cursor-pointer'>
+        <li className='transition-all hover:bg-black rounded-md duration-100 menu-item cursor-pointer'>
             <Link className={`${pathname === "/" ? "text-black" : "text-white"} font-primaryFont`} href="/">Home</Link>
         </li>
-        <li className='transition-all duration-100 hover:border-b-2 cursor-pointer'>
+        <li className='transition-all hover:bg-black rounded-md duration-100 menu-item cursor-pointer'>
             <Link className={`${pathname === "/contact" ? "text-black" : "text-white"} font-primaryFont`} href="/contact">Contact</Link>
         </li>
-        <li className='transition-all duration-100 hover:border-b-2 cursor-pointer'>
+        <li className='transition-all hover:bg-black rounded-md duration-100 menu-item cursor-pointer'>
             <Link className={`${pathname === "/about" ? "text-black" : "text-white"} font-primaryFont`} href="/about">About Us</Link>
         </li>
-        <li className='transition-all duration-100 hover:border-b-2 cursor-pointer'>
+        <li className='transition-all hover:bg-black rounded-md duration-100 menu-item cursor-pointer'>
             <Link className={`${pathname === "/donate" ? "text-black" : "text-white"} font-primaryFont`} href="/donate">Donate</Link>
         </li>
-        <li className='transition-all duration-100 hover:border-b-2 cursor-pointer'>
+        <li className='transition-all hover:bg-black rounded-md duration-100 menu-item cursor-pointer'>
             <Link className={`${pathname === "/quiz" ? "text-black" : "text-white"} font-primaryFont`} href="/quiz">Spiritual Quiz</Link>
         </li>
     </>;
