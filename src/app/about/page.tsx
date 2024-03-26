8c67d926ec66846c7bfe6d6847218bb601c2a33b
Dolon Chandra Roy <dolonr718@gmail.com>
2024-03-26 15:42:57 +0600

    about us page design added commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,46 @@
+import MyContainer from "@/components/MyContainer";
+
+
+const page = () => {
+    return (
+        <>
+            <MyContainer>
+                <p className="mt-10 md:text-xl text-lg font-semibold italic text-center">
+                    “Just like in the material world, there is education for medical man, there is education for engineer, there is education for so many other departments. Similarly, there is education how to make one man spiritual. So we are going to give education how to become spiritually advanced. That is our purpose.”
+                </p>
+                <strong className="block md:text-2xl text-xl font-semibold text-center my-4">– Srila Prabhupada</strong>
+
+                <div>
+                    <p className="md:text-xl text-lg pt-2">
+                        We are trying to give human society an opportunity for a life of happiness, good health, peace of mind and all good qualities through God Consciousness.
+                    </p>
+
+                    <p className="md:text-xl text-lg text-black md:leading-relaxed leading-relaxed">
+                        <strong className="font-bold">What is the mission of human life?</strong> The mission of human life is to end the miseries of material existence and attain a blissful life. We are constantly searching after happiness, but we often fail in our pursuit. We may get a glimpse of happiness, but it does not last forever. We do not want miseries, but we cannot avoid them. Scriptures inform us that we are spiritual beings, part and parcel of the Supreme Lord Sri Krishna, and by nature we are full of happiness – <strong className="font-extrabold">ānandamayo ‘bhyāsāt</strong> (vedānta-sūtra). Then, why do we suffer? How do we rediscover the lost happiness and lead a blissful life? <br />
+
+                        Srila Prabhupada, our beloved spiritual master and the founder-acharya of the International Society for Krishna Consciousness (ISKCON) has blessed us with this opportunity for a happy and peaceful life. We just have to practice the following principles:
+                        <br />
+                        1. Chant the holy names, Hare Krishna maha-mantra every day <br />
+                        2. Read the Srimad Bhagavatam, the spotless purana that glorifies the Supreme Lord <br />
+                        3. Honor prasadam, the food sanctified by offering to the Supreme Lord <br />
+                        4. Just by following the above three principles one can achieve real happiness in life. How does this work? <br />
+                        <br />
+                        First, let us understand the root cause of our problems that makes our life miserable. We are entrapped in this material world from time immemorial and the happiness we pursue here is temporary and illusory. In the pursuit of such happiness, we engage in various activities, pious and sinful, that binds us more and more in this material world.
+
+                        Our miseries are due to the sinful reactions and when we follow these three principles, all the material contaminations and sins are washed away; consequently, we are reestablished in our real constitutional position of unlimited bliss and happiness.
+
+                        Sri Chaitanya Mahaprabhu said: ceto-darpaṇa-mārjanam bhava-mahā-dāvāgni-nirvāpaṇaṁ: Chanting the holy names of the Lord cleanses the mirror of the heart and stops the miseries of the blazing fire of material existence.
+
+                        Srimad Bhagavatam says: naṣṭa-prāyeṣv abhadreṣu nityaṁ bhāgavata-sevayā: All the inauspicious things in our heart are completely destroyed by reading Srimad Bhagavatam every day. The darkness of ignorance is dissipated and we become situated in the mode of goodness and develop all good qualities equivalent to that of the demigods.
+
+                        By honoring prasadam (food offered to the Lord) we do not incur further sinful reactions. Krishna says in the Bhagavad-gita (3.13): yajña-śiṣṭāśinaḥ santo mucyante sarva-kilbiṣaiḥ: devotees who eat food offered first in sacrifice (as an offering to the Lord) are released from all kinds of sins.
+
+                        Thus, by following these three simple, but effective principles given by Srila Prabhupada, we can achieve a life of happiness, good health, peace of mind, and all good qualities.
+                    </p>
+                </div>
+            </MyContainer>
+        </>
+    );
+};
+
+export default page;
