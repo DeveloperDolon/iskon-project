a50214bf039efbb86bb9c7ab50a626fab81a2fd9
Dolon Chandra Roy <dolonr718@gmail.com>
2024-06-29 16:18:11 +0600

    todo slice implemented commit

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -2,10 +2,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import baseApi from "./features/baseApi";
 // import userSlice from "./features/userAuth.ts";
+import todoReducer from "./features/todo/todoSlice";
 
 export const store = configureStore({
   reducer: {
     // userSlice: userSlice,
+    todos: todoReducer,
     [baseApi.reducerPath]: baseApi.reducer,
   },
 
@@ -13,3 +15,6 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(baseApi.middleware),
 });
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
