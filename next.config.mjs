b94afe0728d076e2792ae993c4c81f559d936f81
DeveloperDolon <dolonr718@gmail.com>
2024-10-14 17:58:32 +0600

    banner image changed commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['selfrealization.in'],
-    },
-    webpack: (config, { isServer }) => {
-        // Add the file-loader for mp3 files
-        config.module.rules.push({
-          test: /\.mp3$/,
-          use: {
-            loader: 'file-loader',
-            options: {
-              publicPath: '/_next',
-              name: 'static/media/[name].[hash].[ext]',
-            },
-          },
-        });
-    
-        // Important: return the modified config
-        return config;
+  images: {
+    domains: ["selfrealization.in"],
+  },
+  webpack: (config, { isServer }) => {
+    // Add the file-loader for mp3 files
+    config.module.rules.push({
+      test: /\.mp3$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          publicPath: "/_next",
+          name: "static/media/[name].[hash].[ext]",
+        },
       },
+    });
+
+    // Important: return the modified config
+    return config;
+  },
 };
 
 export default nextConfig;
--- src/components/HeroBanner.tsx
+++ src/components/HeroBanner.tsx
@@ -1,16 +1,16 @@
-
+import bannerImage from "@/assets/bannerImg.jpg";
 
 const HeroBanner = () => {
-    return (
-        <div 
-            className="md:h-[87vh] sm:h-[400px] h-[300px]"
-            style={{
-                background: `url(https://selfrealization.in/wp-content/uploads/2022/08/Welcome-to-Self-Realization-Course.jpg) top center no-repeat`,
-                backgroundSize: "contain"
-            }}
-        >           
-        </div>
-    );
+  return (
+    <div
+      className="md:h-[87vh] sm:h-[400px] h-[300px]"
+      style={{
+        background: `url(${bannerImage.src})`,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+      }}
+    ></div>
+  );
 };
 
 export default HeroBanner;
